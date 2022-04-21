9ca5a965d7bacbd667b2ba3626a3ade8dc7c6450
alexey <a.chemakin72@gmail.com>
2022-04-21 12:11:02 +0300

    initial

---
+++ src/main/java/ru/lexa/books_rewiews/BooksRewiewsApplication.java
@@ -0,0 +1,13 @@
+package ru.lexa.books_rewiews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRewiewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksRewiewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lexa/books_rewiews/BooksRewiewsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lexa.books_rewiews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRewiewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
