85c02dbfdc4793311d65884e088ff895abf4fe17
alexey <a.chemakin72@gmail.com>
2022-04-21 12:26:02 +0300

    add models

---
+++ src/main/java/ru/lexa/books_rewiews/model/Book.java
@@ -0,0 +1,29 @@
+package ru.lexa.books_rewiews.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "book_id", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Review> review;
+
+//    @Column(unique = true, nullable = false)
+//    @NotNull
+    private String name;
+    private String description;
+    private String author;
+}
---
+++ src/main/java/ru/lexa/books_rewiews/model/Review.java
@@ -0,0 +1,28 @@
+package ru.lexa.books_rewiews.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String text;
+
+    @Min(1)
+    @Max(10)
+    private int rating;
+    @Column(nullable = false)
+    private long book_id;
+}
