93a0c31423954b7edba23c536da9178412926251
mqueen21 <a.Chemakin72@gmail.com>
2022-05-05 13:16:27 +0300

    improve controller

--- src/main/java/ru/lexa/books_reviews/controller/BookController.java
+++ src/main/java/ru/lexa/books_reviews/controller/BookController.java
@@ -1,24 +1,47 @@
 package ru.lexa.books_reviews.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.lexa.books_reviews.controller.dto.BookDTO;
 import ru.lexa.books_reviews.controller.dto.ReviewDTO;
 
 import javax.validation.Valid;
 import java.util.Collection;
 
+
+@RequestMapping("/api/books")
 public interface BookController {
 
-    BookDTO createBook(@Valid BookDTO dto);
+	@ApiOperation(value = "Добавить новую книгу.")
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	BookDTO createBook(@RequestBody @Valid BookDTO dto);
 
-    Collection<BookDTO> readAll(String author, String description, String name, String reviewText);
+	@ApiOperation(value = "Получить все книги.")
+	@GetMapping
+	Collection<BookDTO> readAll(@RequestParam(required = false) String author,
+	                            @RequestParam(required = false) String description,
+	                            @RequestParam(required = false) String name,
+	                            @RequestParam(required = false) String reviewText);
 
-    BookDTO readBook(long id);
+	@ApiOperation(value = "Получить книгу.")
+	@GetMapping("/{id}")
+	BookDTO readBook(@PathVariable  long id);
 
-    BookDTO updateBook(BookDTO dto, long id);
+	@ApiOperation(value = "Изменить книгу.")
+	@PutMapping("/{id}")
+	BookDTO updateBook(@RequestBody BookDTO dto, @PathVariable long id);
 
-    void deleteBook(long id);
+	@ApiOperation(value = "Удалить книгу.")
+	@DeleteMapping("/{id}")
+	void deleteBook(@PathVariable long id);
 
-    Collection<ReviewDTO> getReviews(long id);
+	@ApiOperation(value = "Получить отзывы к книге.")
+	@GetMapping("/{id}/reviews")
+	Collection<ReviewDTO> getReviews(@PathVariable long id);
 
-    double getAverage(long id);
+	@ApiOperation(value = "Получить среднюю оценку.")
+	@GetMapping("/{id}/averageRating")
+	double getAverage(@PathVariable long id);
 }
--- src/main/java/ru/lexa/books_reviews/controller/implementation/BookControllerImpl.java
+++ src/main/java/ru/lexa/books_reviews/controller/implementation/BookControllerImpl.java
@@ -1,9 +1,7 @@
 package ru.lexa.books_reviews.controller.implementation;
 
-import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import ru.lexa.books_reviews.controller.BookController;
 import ru.lexa.books_reviews.controller.dto.BookDTO;
 import ru.lexa.books_reviews.controller.dto.BookFilterDTO;
@@ -15,68 +13,50 @@ import ru.lexa.books_reviews.service.BookService;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/api/books")
 @AllArgsConstructor
+@RestController
 public class BookControllerImpl implements BookController {
 	private final BookService bookService;
 
 	private ReviewMapper reviewMapper;
 	private BookMapper bookMapper;
 
-	@ApiOperation(value = "Добавить новую книгу.")
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
 	@Override
-	public BookDTO createBook(@RequestBody BookDTO dto) {
+	public BookDTO createBook(BookDTO dto) {
 		return bookMapper.bookToDto(bookService.create(bookMapper.dtoToBook(dto)));
 	}
 
-	@ApiOperation(value = "Получить все книги.")
-	@GetMapping
 	@Override
-	public Collection<BookDTO> readAll(@RequestParam(required = false) String author,
-	                                   @RequestParam(required = false) String description,
-	                                   @RequestParam(required = false) String name,
-	                                   @RequestParam(required = false) String reviewText) {
+	public Collection<BookDTO> readAll(String author, String description, String name, String reviewText) {
 		BookFilterDTO filter = new BookFilterDTO(name, description, author, reviewText);
 		return bookService.readAll(filter).stream().map(bookMapper::bookToDto).collect(Collectors.toList());
 	}
 
-	@ApiOperation(value = "Получить книгу.")
-	@GetMapping("/{id}")
 	@Override
-	public BookDTO readBook(@PathVariable long id) {
+	public BookDTO readBook(long id) {
 		return bookMapper.bookToDto(bookService.read(id));
 	}
 
-	@ApiOperation(value = "Изменить книгу.")
-	@PutMapping("/{id}")
 	@Override
-	public BookDTO updateBook(@RequestBody BookDTO dto, @PathVariable long id) {
+	public BookDTO updateBook(BookDTO dto, long id) {
 		dto.setId(id);
 		return bookMapper.bookToDto(bookService.update(bookMapper.dtoToBook(dto)));
 	}
 
-	@ApiOperation(value = "Удалить книгу.")
-	@DeleteMapping("/{id}")
 	@Override
-	public void deleteBook(@PathVariable long id) {
+	public void deleteBook(long id) {
 		bookService.delete(id);
 	}
 
-	@ApiOperation(value = "Получить отзывы к книге.")
-	@GetMapping("/{id}/reviews")
 	@Override
-	public Collection<ReviewDTO> getReviews(@PathVariable long id) {
+	public Collection<ReviewDTO> getReviews(long id) {
 		return bookService.read(id).getReview().stream()
 				.map(reviewMapper::reviewToDto)
 				.collect(Collectors.toList());
 	}
 
-	@ApiOperation(value = "Получить среднюю оценку.")
-	@GetMapping("/{id}/averageRating")
-	public double getAverage(@PathVariable long id) {
+	@Override
+	public double getAverage(long id) {
 		return bookService.averageRating(id);
 	}
 }
